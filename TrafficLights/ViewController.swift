62cc0ebc9d541d068e294e8c7d1b453e6772e297
id2k1149 <id2k1149@gmail.com>
2022-10-25 10:04:13 -0400

    Initial Commit

---
+++ TrafficLights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLights
+//
+//  Created by Max Franz Immelmann on 10/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
